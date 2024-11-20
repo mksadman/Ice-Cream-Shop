459f266236572c1c8544ed1c62aa667d760123ce
Md. Sadman Mubasshir Khan <sadmankhan@iut-dhaka.edu>
2024-11-21 00:44:17 +0600

    polishes

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
         repository.addTopping(new Topping("Sprinkles", 0.30));
         repository.addTopping(new Topping("Fresh Strawberries", 1.00));
 
-        Order order = new Order(true); // Waffle cone
-        order.addFlavor(repository.getFlavors().get(0)); // Mint Chocolate Chip
-        order.addFlavor(repository.getFlavors().get(1)); // Chocolate Fudge
-        order.addTopping(repository.getToppings().get(0)); // Sprinkles
-        order.addTopping(repository.getToppings().get(1)); // Fresh Strawberries
+        Order order = new Order(true);
+        order.addFlavor(repository.getFlavors().get(0));
+        order.addFlavor(repository.getFlavors().get(1));
+        order.addTopping(repository.getToppings().get(0));
+        order.addTopping(repository.getToppings().get(1));
 
         InvoiceGenerator.generateInvoice(order, "invoice.txt");
     }
